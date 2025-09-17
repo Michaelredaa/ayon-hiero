ef45007ad491b0588552bbd8401df731bc4e06b3
Michael Reda <eng.michaelreda@gmail.com>
2025-09-17 21:57:52 +1000

    Revert Add default tasks for shot creation

--- client/ayon_hiero/plugins/publish/collect_tag_tasks.py
+++ client/ayon_hiero/plugins/publish/collect_tag_tasks.py
@@ -1,9 +1,7 @@
 from pyblish import api
-from ayon_core.settings import get_current_project_settings
 import json
 
 
-DEFAULT_TASKS = ["compositing"]
 class CollectClipTagTasks(api.InstancePlugin):
     """Collect Tags from selected track items."""
 
@@ -16,8 +14,9 @@ class CollectClipTagTasks(api.InstancePlugin):
         # gets tags
         tags = instance.data["tags"]
 
-        instance.data["tasks"] = {}
+        tasks = {}
         for tag in tags:
+
             t_metadata = dict(tag.metadata())
             if t_metadata.get("tag.json_metadata"):
                 metadata = json.loads(t_metadata["tag.json_metadata"])
@@ -25,7 +24,7 @@ class CollectClipTagTasks(api.InstancePlugin):
                 if product_type == "task":
                     task_type = metadata.get("type")
                     task_name = t_metadata.get("tag.label", "")
-                    instance.data["tasks"][task_name] = {"type": task_type}
+                    tasks[task_name] = {"type": task_type}
             else:
                 # backward compatiblity for older tags
                 t_product_type = t_metadata.get("tag.productType")
@@ -36,22 +35,10 @@ class CollectClipTagTasks(api.InstancePlugin):
                 if "task" in t_product_type:
                     t_task_name = t_metadata.get("tag.label", "")
                     t_task_type = t_metadata.get("tag.type", "")
-                    instance.data["tasks"][t_task_name] = {"type": t_task_type}
-        self.log.debug(f"collected tasks:: {instance.data['tasks']}")
-        
-        for task in self.get_default_tasks():
-            instance.data["tasks"].update({
-                task: {"type": task.capitalize()}
-            })
+                    tasks[t_task_name] = {"type": t_task_type}
+
+        instance.data["tasks"] = tasks
 
         self.log.info("Collected Tasks from Tags: `{}`".format(
             instance.data["tasks"]))
-
-    def get_default_tasks(self):
-        settings = get_current_project_settings()
-        core_settings = settings["core"]
-        for filter in core_settings.get("filter_env_profiles", []):
-            if "default_conform_tasks" in filter.get("host_names", []):
-                return filter.get("task_names", []) or DEFAULT_TASKS
-                    
-        return DEFAULT_TASKS
+        return
